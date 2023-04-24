1f572b726050d3f654c1aae9c7cfee9cd5f0758f
darongit <darolbk@gmail.com>
2023-04-24 19:20:06 +0200

    Add scrollbar and background color for included letters, code cleanup

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,7 @@
+var validLettersGlobal = {};
+var includedGlobal;
+var excludedGlobal;
+
 function emptyFields() {
     var html = "<h1>WordleSoler<h1>" +
     "<h2>Enter the letters in the box on the left and press Check!<h2>";
@@ -8,29 +12,70 @@ function start() {
     emptyFields();
 }
 
+function updateGlobals() {
+    validLettersGlobal = {};
+    if (document.getElementById("l0").value != "") {
+        validLettersGlobal[0] = document.getElementById("l0").value.toLowerCase();
+    }
+    if (document.getElementById("l1").value != "") {
+        validLettersGlobal[1] = document.getElementById("l1").value.toLowerCase();
+    }
+    if (document.getElementById("l2").value != "") {
+        validLettersGlobal[2] = document.getElementById("l2").value.toLowerCase();
+    }
+    if (document.getElementById("l3").value != "") {
+        validLettersGlobal[3] = document.getElementById("l3").value.toLowerCase();
+    }
+    if (document.getElementById("l4").value != "") {
+        validLettersGlobal[4] = document.getElementById("l4").value.toLowerCase();
+    }
+    excludedGlobal = document.getElementById("excludedLetters").value.toLowerCase();
+    includedGlobal = document.getElementById("includedLetters").value.toLowerCase();    
+}
+
+function colorWord(word) {
+    var result = "";
+    var tmp = "";
+    var letter;
+    for (var i=0; i<word.length; i++) {
+        letter = word.charAt(i);
+        if (!includedGlobal.includes(letter) && validLettersGlobal[i]!=letter) {
+            result += letter;
+            continue;
+        }
+        if (validLettersGlobal[i] == letter) {
+            result += "<span class=\"table-valid-letter\">" + letter + "</span>";
+        } else {
+            result += "<span class=\"table-included-letter\">" + letter + "</span>";
+        }
+         
+    }
+    return result;
+}
+
 function showTable(wordsList) {
+    // console.log(validLettersGlobal);
+    // console.log(includedGlobal);
+    // console.log(excludedGlobal);
     var html = "<table align=\"center\">";
-    var lineLimit = 6;
+    var lineLimit = 8;
     var idx = 0;
     if (wordsList.length == 0) {
         html += "<tr><td>No correct words:(</td></tr>";
     }
     else if (wordsList.length == 1) {
-        html += "<tr><td>" + wordsList[0] + "</td></tr>";
+        html += "<tr><td>" + colorWord(wordsList[0]) + "</td></tr>";
     } else {
         while (idx < wordsList.length) {
             html += "<tr>"
             for (var i=0; i<lineLimit; i++) {
-                html += "<td>" + wordsList[idx] + "</td>";
+                html += "<td>" + colorWord(wordsList[idx]) + "</td>";
                 idx++;
                 if (idx > wordsList.length-1) {
                     break;
                 }
             }
             html += "</tr>"
-            if (idx == wordsList.length-1) {
-                break;
-            }
         }
     }
 
@@ -39,36 +84,18 @@ function showTable(wordsList) {
 }
 
 function buttonOnClick() {
+    updateGlobals();
     var query = {};
-    var validLetters = {};
-    var words;
-
-    if (document.getElementById("l0").value != "") {
-        validLetters[0] = document.getElementById("l0").value.toLowerCase();
-    }
-    if (document.getElementById("l1").value != "") {
-            validLetters[1] = document.getElementById("l1").value.toLowerCase();
-    }
-    if (document.getElementById("l2").value != "") {
-        validLetters[2] = document.getElementById("l2").value.toLowerCase();
-    }
-    if (document.getElementById("l3").value != "") {
-            validLetters[3] = document.getElementById("l3").value.toLowerCase();
-    }
-    if (document.getElementById("l4").value != "") {
-        validLetters[4] = document.getElementById("l4").value.toLowerCase();
-    }
-
-    if (Object.keys(validLetters).length == 0
-        && document.getElementById("excludedLetters").value.length == 0
-        && document.getElementById("includedLetters").value.length == 0) {
+    if (Object.keys(validLettersGlobal).length == 0
+        && excludedGlobal.length == 0
+        && includedGlobal.length == 0) {
             emptyFields();
             return;
         }
 
-    query["excluded"] = document.getElementById("excludedLetters").value.toLowerCase();
-    query["included"] = document.getElementById("includedLetters").value.toLowerCase();
-    query["validLetters"] = validLetters;
+    query["excluded"] = excludedGlobal;
+    query["included"] = includedGlobal;
+    query["validLetters"] = validLettersGlobal;
     fetch('api/getwords', {
       method: 'POST',
       headers: {
