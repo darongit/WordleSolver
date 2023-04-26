a32fb02c5f19e68f0733349a90049e76763149ff
darongit <darolbk@gmail.com>
2023-04-26 18:28:04 +0200

    Create events, fix some bugs

--- src/main/resources/static/js/events.js
+++ src/main/resources/static/js/events.js
@@ -1,16 +1,102 @@
-// const validLettersSelectors = document.querySelectorAll(".valid-letter");
-// const includedSelector = document.querySelector("#includedLetters");
-// const excludedSelector = document.querySelector("#excludedLetters");
-// const alphabet = "abcdefghijklmnopqrstuvwxyząęłóżźć".split("");
+const validLettersSelectors = document.querySelectorAll(".valid-letter");
+const includedSelector = document.querySelector("#includedLetters");
+const excludedSelector = document.querySelector("#excludedLetters");
 
-// function removeDuplicates(word) {
-//     var resultSet = new Set(word.toLowerCase().split());
-//     return Array.from(resultSet).join("");
-// }
+function convertPolishChar(letter) {
+    return letter.replace(/ą/g,"a")
+                     .replace(/ć/g,"c")
+                     .replace(/ę/g,"e")
+                     .replace(/ł/g,"l")
+                     .replace(/ń/g,"n")
+                     .replace(/ó/g,"o")
+                     .replace(/ś/g,"s")
+                     .replace(/(ż|ź)/g,"z")
+                     .replace(/Ą/g,"A")
+                     .replace(/Ć/g,"C")
+                     .replace(/Ę/g,"E")
+                     .replace(/Ł/g,"L")
+                     .replace(/Ń/g,"N")
+                     .replace(/Ó/g,"O")
+                     .replace(/Ś/g,"S")
+                     .replace(/(Ż|Ź)/g,"Z");
+  }
 
-// function onPress(event) {
-//     event.target.value = removeDuplicates(event.target.value + event.key);
-// }
+function removeNonAlphabeticalChars(event) {
+    var keycode = event.which || event.keyCode;
+    var polishCharConverted = convertPolishChar(event.key).charCodeAt(0);
+        if(((keycode < 58 || 
+        (keycode > 57 && keycode < 65) || 
+        (keycode > 90 && keycode < 97) ||
+        keycode > 122) && keycode == polishCharConverted)
+       ){
+           event.preventDefault();  
+           return false;
+       }
+       return true;
+}
 
-// includedSelector.addEventListener("keypress", (event) => onPress(event));
-// excludedSelector.addEventListener("keypress", (event) => onPress(event));
+function validLettersSwitch(event) {
+    var key = event.key;
+    var oldKey = event.target.value;
+    console.log();
+    if (key != oldKey && preventDuplicates(event, excludedSelector.value && event.keyCode && removeNonAlphabeticalChars(event))) {
+        event.target.value = key;
+    } else {
+        event.target.value = oldKey;
+    }
+}
+
+
+function preventDuplicates(event, lettersSet) {
+    var key = event.key;
+    lettersSet += lettersSet.toUpperCase();
+    if (lettersSet.includes(key)) {
+        event.preventDefault();  
+        return false;
+    }
+    return true;
+}
+
+function pressEnter(event) {
+    if (event.keyCode == 13) {
+        buttonOnClick();
+    }
+}
+
+function getValidLetters() {
+    var validLettersString = "";
+    var validLetter;
+    for (var i=0; i < validLettersSelectors.length; i++) {
+        validLetter = validLettersSelectors[i].value.trim();
+        if (!validLetter == "") {
+            validLettersString += validLetter;
+        }
+    }
+    return validLettersString;
+}
+
+function changeValidBackground(event) {
+    if (event.target.value == "" || event.target.value == " ") {
+        event.target.style.background = "white";
+    } else {
+        event.target.style.background = "yellowgreen";
+    }
+}
+
+window.addEventListener("keydown", (event) => pressEnter(event));
+
+includedSelector.addEventListener("keypress", (event) => removeNonAlphabeticalChars(event));
+excludedSelector.addEventListener("keypress", (event) => removeNonAlphabeticalChars(event));
+
+includedSelector.addEventListener("keypress", 
+    (event) => preventDuplicates(event, includedSelector.value + excludedSelector.value));
+excludedSelector.addEventListener("keypress", 
+    (event) => preventDuplicates(event, includedSelector.value + excludedSelector.value + getValidLetters()));
+
+for (var i=0; i<validLettersSelectors.length; i++) {
+    validLettersSelectors[i].addEventListener("keypress", (event) => removeNonAlphabeticalChars(event));
+    validLettersSelectors[i].addEventListener("keypress", 
+        (event) => preventDuplicates(event, excludedSelector.value));
+    validLettersSelectors[i].addEventListener("keyup", (event) => changeValidBackground(event));
+    validLettersSelectors[i].addEventListener("keypress", (event) => validLettersSwitch(event));
+}
